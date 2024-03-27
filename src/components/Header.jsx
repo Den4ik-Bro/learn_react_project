2b29786ba880d3a731ff6a7c0fe46fc4a5248f57
Den4ik-Bro <denis.rr@bk.ru>
2024-03-27 17:36:17 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ function Header() {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="#!" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="https://github.com/Den4ik-Bro" target="_blank">
+            <a href="https://github.com/Den4ik-Bro/learn_react_shop_project" target="_blank" rel="noreferrer">
               Repo
             </a>
           </li>
